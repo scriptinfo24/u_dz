55ff5c200d818d096478834225d6047320449882
As <scriptinfo24@gmail.com>
2024-11-01 21:06:57 +0700

    primes2.5

--- 2/module_2_5.py
+++ 2/module_2_5.py
@@ -2,11 +2,12 @@ def get_matrix(n, m, value):
 
     for i in range(n):
         matrix = [value]*m
-        print(matrix, end=" ")
+        return matrix
 
 result1 = get_matrix(2, 2, 10)
-print(result1, sep='\n')
 result2 = get_matrix(3, 5, 42)
-print(result2, sep='\n')
 result3 = get_matrix(4, 2, 13)
-print(result3, sep='\n')
+
+print(result1)
+print(result2)
+print(result3)
